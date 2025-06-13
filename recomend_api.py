340a90006927c718c7925bb8e081a6c2452fac14
AndhikaArdi <127767610+AndhikaArdi@users.noreply.github.com>
2025-06-13 09:45:42 +0700

    configuring port

--- recomend_api.py
+++ recomend_api.py
@@ -38,4 +38,7 @@ def home():
     return jsonify({"message": "SkinSync Backend is running!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+    # backend berjalan di port 5000
