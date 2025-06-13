b245698433f81628e3c30acfd63a6587261546f6
AndhikaArdi <127767610+AndhikaArdi@users.noreply.github.com>
2025-06-13 09:29:46 +0700

    minor fix 'typo'
    typo import recommend di recomend_api.py

--- recomend_api.py
+++ recomend_api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
-from utils.recomend import get_recommendation
+from utils.recommend import get_recommendation
 
 app = Flask(__name__)
 CORS(app)
